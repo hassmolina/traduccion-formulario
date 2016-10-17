a53caded43b9b4987159734d6b3568c74b3ef2ce
hassmolina <hassmolinagutierrez@gmail.com>
2016-10-17 16:59:22 -0500

    ejercicios2

---
+++  traduccion-formulario /js/main.js
@@ -0,0 +1,7 @@
+function changelanguage() {
+    document.getElementById('form-signin-heading').innerHTML = "Por favor inicia sesión";
+    document.getElementById('inputEmail').value = "Correo Electrónico";
+    document.getElementById('inputPassword').placeholder = "Contraseña";
+    document.querySelector("span").innerHTML = "Recordar datos";
+    document.querySelector("").innerHTML = "Iniciar Sesión";
+}
---
+++ bicis locas/bicis-locas/js/main.js
@@ -0,0 +1,35 @@
+function validateForm(){
+	var nombre = document.getElementById("name").value;
+	if(nombre == ""||nombre == null){
+		alert("Digite el usuario");
+	return false;
+	}
+	  if (!/^[a-zA-Z]*$/g.test(document.getElementById("name"))) {
+        alert("Invalid characters");
+        return false;
+    }
+	var apellido = document.getElementById("lastname").value;
+	if(apellido == ""||apellido == null){
+		alert("Digite el apellido");
+	return false;
+	}
+	var correo = document.getElementById("input-email").value;
+	if(correo == ""||correo == null){
+ 		alert("Digite su correo");
+ 	return false;
+ 	}
+	var contraseña = document.getElementById("input-password").value;
+	if(constraseña == ""||contraseña == null){
+ 		alert("Digite su contraseña");
+ 	return false;
+ 	}
+
+
+ // DEBE TENER MENOS DE 6 CARACTERES
+ 	var contra = document.getElementById("input-password").value;
+ 	if(contra.length<5){
+ 		alert("Contraseña al menos 6 caracteres");
+ 	}else if(contra=="123456"||contra=="password"||contra=="098754"){
+ 		alert("por favor escribe otra");
+ 	}
+}
